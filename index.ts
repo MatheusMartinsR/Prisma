b73c42fd879b4508611b778186de0e879d9b6e45
MatheusMartinsR <martinsmatheusrh08@gmail.com>
2023-10-27 21:18:33 -0300

    creating another consts

--- index.ts
+++ index.ts
@@ -1,6 +1,46 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
+(async () => {
+    const pessoa1 = await prisma.pessoa.create({
+        data: {
+            name: "Maria Eduarda",
+            email: "teste@teste.com",
+        },
+    });
+    console.log(pessoa1)
+})();
+
+(async () => {
+    const pessoas = await prisma.pessoa.count();
+    console.log(`Quantidade de Pessoas no banco:  ${pessoas}`)
+})();
+
+(async () => {
+    const pessoas = await prisma.pessoa.findMany({
+        where:{
+            name:{
+                startsWith: "M",
+            },
+        },
+    });
+    console.log(pessoas);
+})();
+
+(async () => {
+    const pessoaComEndereco = await prisma.pessoa.create({
+        data: {
+            name:"Pessoa nova",
+            email: "com1@endereco.com",
+            enderecos:{
+                create:{
+                    rua: "Rua Exemplo",
+                },
+            },
+        },
+    });
+    console.log(pessoaComEndereco);
+})();
 
 
 
